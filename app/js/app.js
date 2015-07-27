3b4556304754ac980ec05746bb1758c3421fd4d8
Philip A. Hirt <phirt13@gmail.com>
2015-07-27 16:44:39 -0700

    angular start

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,18 @@
-var x = 12;
+'use strict';
+
+require('angular/angular');
+require('angular-route');
+
+var friendingLibrary = angular.module('friendingLibrary', ['ngRoute']);
+
+//controllers
+require('./welcome/controllers/welcome_controller.js')(friendingLibrary);
+
+//routes
+friendingLibrary.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/views/welcome.html',
+      controller: 'welcomeController'
+    });
+}]);
