a622f31f78501c8ad12289b03f29020034c77520
Colin Coltrera <c.coltrera@gmail.com>
2015-07-28 11:29:26 -0700

    Move gulp-db to separate file, add Book to database.

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,9 @@ var webpack = require('gulp-webpack');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
 var minifyHTML = require('gulp-minify-html');
-var mongoose = require("mongoose");
 var mocha = require("gulp-mocha");
+
+var gulpDB = require("./gulp-db");
 
 gulp.task('sass', function() {
   gulp.src('./app/sass/**/*.scss')
@@ -58,57 +59,6 @@ gulp.task("test", function() {
     .pipe(mocha({"reporter" : "nyan"}));
 });
 
-gulp.task("dbSetup", function() {
-  var user1 = {
-    _id: "12345",
-    displayName: "Philip",
-    access_token: "v583ms",
-    books: [],
-    borrowing: []
-  };
-  var user2 = {
-    _id: "16829",
-    displayName: "David",
-    access_token: "cvasr32",
-    books: [],
-    borrowing: []
-  };
-  var user3 = {
-    _id: "02486",
-    displayName: "Brandon",
-    access_token: "kjlewq02",
-    books: [],
-    borrowing: []
-  };
-  var user4 = {
-    _id: "08473",
-    displayName: "Colin",
-    access_token: "uiowqe879803",
-    books: [],
-    borrowing: []
-  };
+gulp.task("dbSetup", gulpDB.dbSetup);
 
-  var User = require("./models/User");
-  mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/friending-library");
-
-  User.create(user1, function(err,user1) {
-    User.create(user2, function(err,user2) {
-      User.create(user3, function(err,user3) {
-        User.create(user4, function(err,user4) {
-          console.log("users added");
-          mongoose.connection.close();
-        });
-      });
-    });
-  });
-});
-
-gulp.task("dbBreakdown", function() {
-  mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/friending-library");
-  mongoose.connection.once("open", function() {
-    mongoose.connection.db.dropDatabase(function() {
-      console.log("db dropped");
-      mongoose.connection.close();
-    });
-  });
-});
+gulp.task("dbBreakdown", gulpDB.dbBreakdown);
