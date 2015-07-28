3912ba76fbd4a1b35cfbfd66e75383183838b358
Philip A. Hirt <phirt13@gmail.com>
2015-07-28 14:56:12 -0700

    working REST service for users

--- app/js/auth/services/auth_resource.js
+++ app/js/auth/services/auth_resource.js
@@ -4,28 +4,42 @@ module.exports = function(app) {
 
   app.factory('authResource', ['$http', '$cookies', function($http, $cookies) {
     return function() {
-      logIn: function() {
-        $http({
-          method: 'GET',
-          url: '/auth/facebook'
-          //Talk to back-end I'm not a fan maybe 'api/auth/facebook' doesnt matter too much
-        })
-        .success(function(data) {
-          console.log(data);
-          $cookies.put('eat', data.user.access_token);
-        })
-        .error(function(data) {
-          console.log(data);
-        });
-      },
+      return {
+      logIn: function(data) {
+          $http({
+            method: 'GET',
+            url: '/auth/facebook'
+          })
+          .success(function(data) {
+            console.log(data);
+            // $cookies.put('eat', data.user.access_token);
+          })
+          .error(function(data) {
+            console.log(data);
+          });
+        },
 
-      logout: function() {
-        $cookies.put('eat', '');
-      },
+        testLogin: function(data, err) {
+          $http({
+            method: 'GET',
+            url: '/api/test'
+          })
+          .success(function(data) {
+            console.log(data);
+          })
+          .error(function(err) {
+            console.log(err);
+          })
+        },
 
-      isSignedIn: function() {
-        return ($cookies.get('eat') && $cookies.get('eat').length);
-      }
+        logout: function() {
+          $cookies.put('eat', '');
+        },
+
+        isSignedIn: function() {
+          return ($cookies.get('eat') && $cookies.get('eat').length);
+        }
+      };
     };
   }]);
 
