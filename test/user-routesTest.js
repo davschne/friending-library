6b3eb387fbb4da66f7cced32bc0cf4c4d0e9e4bc
Colin Coltrera <c.coltrera@gmail.com>
2015-07-28 15:45:54 -0700

    Add DELETE method to api/users, which deletes a User and all the Books they own.

--- test/user-routesTest.js
+++ test/user-routesTest.js
@@ -7,36 +7,58 @@ var app = require("../server");
 chai.use(require("chai-http"));
 
 var User = require("../models/User");
+var Book = require("../models/Book")
 
-var dummyUser = {
-  _id: "Dummy",
-  displayName: "Idiot",
-  access_token: "jerkface"
-}
+var testData = require("../lib/test-data");
+var testBooks = testData.books;
+var testUsers = testData.users;
 
 describe("/api/users", function() {
   before(function(done) {
-    User.create(dummyUser, function(err, data) {
-      if (!err) done();
-    })
+    User.create(testUsers[0], function(err, data) {
+      if (!err) {
+        testBooks[0].owner = testUsers[0]._id;
+        Book.create(testBooks[0], function(err, data) {
+          done();
+        });
+      }
+    });
   });
 
   describe("GET", function() {
     it("should return an existent User as JSON", function(done) {
       chai.request(url)
         .get("/api/users")
-        .set("Authorization", "Bearer " + dummyUser.access_token)
+        .set("Authorization", "Bearer " + testUsers[0].access_token)
         .end(function(err, res) {
           expect(res).to.have.status(200);
           expect(res).to.be.json;
-          expect(res.body._id).to.eql(dummyUser._id);
+          expect(res.body._id).to.eql(testUsers[0]._id);
           done();
         });
     });
   });
 
+  describe("DELETE", function() {
+    it("should delete a User and all of their Books from the database", function(done) {
+      chai.request(url)
+        .del("/api/users")
+        .set("Authorization", "Bearer " + testUsers[0].access_token)
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          User.findOne({_id: testUsers[0]._id}, function(err, userDoc) {
+            expect(userDoc).to.be.null;
+            Book.find({owner: testUsers[0]._id}, function(err, bookDocs) {
+              expect(bookDocs.length).to.eql(0);
+              done();
+            })
+          });
+        });
+    });
+  });
+
   after(function(done) {
-    User.findByIdAndRemove(dummyUser._id, function(err, data) {
+    User.findByIdAndRemove(testUsers[0]._id, function(err, data) {
       if (!err) done();
     });
   });
