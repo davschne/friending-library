be359cd04c68ebd79791753c94c04bbb5d3ea636
Philip A. Hirt <phirt13@gmail.com>
2015-07-28 00:06:56 -0700

    services CRUD + Auth

--- app/js/app.js
+++ app/js/app.js
@@ -4,15 +4,20 @@ require('angular/angular');
 require('angular-route');
 require('angular-cookies');
 
-var friendingLibrary = angular.module('friendingLibrary', ['ngRoute', 'ngCookes']);
+var friendingLibrary = angular.module('friendingLibrary', ['ngRoute', 'ngCookies']);
+
+//services
+require('./auth/services/auth_resource.js')(friendingLibrary);
+require('./services/CRUD_resource.js')(friendingLibrary);
 
 //controllers
 require('./welcome/controllers/welcome_controller.js')(friendingLibrary);
+require('./auth/controllers/auth_controller.js')(friendingLibrary);
 
 //routes
 friendingLibrary.config(['$routeProvider', function($routeProvider) {
   $routeProvider
-    .when('/', {
+    .when('/another', {
       templateUrl: 'templates/views/welcome.html',
       controller: 'welcomeController'
     });
