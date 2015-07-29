eb26ec9a084f2be8648b296fbd2583ff12332931
davschne <davschne@gmail.com>
2015-07-29 12:14:02 -0700

    Made title and author required fields in Book schema

--- models/Book.js
+++ models/Book.js
@@ -6,9 +6,9 @@ var bookSchema = new mongoose.Schema({
   requests: {type: String, ref: "User"}, // ONLY ALLOW ONE REQUEST
   borrower: {type: String, ref: "User"},
   dateBorrowed: Date,
-  title: String,
+  title: {type: String, required: true},
   subtitle: String,
-  authors: [String],
+  authors: [{type: String, required: true}],
   publisher: String,
   publishedDate: String,
   description: String,
