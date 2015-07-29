7305bc782931d9acd6157fd4bb47eddebedd07d3
Colin Coltrera <c.coltrera@gmail.com>
2015-07-29 15:02:07 -0700

    Add proper handling for requests on possible books, with associated test.

--- routes/trans-routes.js
+++ routes/trans-routes.js
@@ -10,31 +10,30 @@ module.exports = function(router) {
       Book.findById(req.body._id, function(err, bookDoc) {
         if (err) {
             handle[500](err, res);
-          } else if (bookDoc == null) {
-            res.sendStatus(404);
+        } else if (bookDoc == null) {
+          res.sendStatus(404);
+        } else {
+          // Check if book is borrowed or requested
+          if (bookDoc.borrower || bookDoc.request) {
+            res.sendStatus(409);
           } else {
-            // Check if book is borrowed or requested
-            if (bookDoc.borrower || bookDoc.request) {
-              res.sendStatus(409);
-            } else {
-              res.json(bookDoc);
-            }
-
-            //else {
-            //   Book.findByIdAndUpdate(req.body._id, {request: req.user._id})
-            //     .exec()
-            //     .then(function() {
-            //       User.findAndUpdate(req.user._id, {$push: {requests : req.body._id}})
-            //         .exec();
-            //     }, function(err) { handle[500](err, res); })
-            //     .then(function() {
-            //       res.json(bookDoc);
-            //     }, function(err) { handle[500](err, res); })
-            // }
+            // Update the user
+            User.update(req.user._id, {$push: {requests : req.body._id}}, function(err) {
+              if (err) {
+                  handle[500](err, res);
+              } else {
+                Book.findByIdAndUpdate(req.body._id, {request: req.user._id}, function(err) {
+                  if (err) {
+                      handle[500](err, res);
+                  } else {
+                    res.json(bookDoc);
+                  }
+                });
+              }
+            });
           }
+        }
       });
-
-
     })
     .delete(function(req, res) {
 
