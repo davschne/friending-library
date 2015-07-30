f705be52d8fdec89e6b9c732633630a260a9d182
davschne <davschne@gmail.com>
2015-07-30 15:35:52 -0700

    Initial work on Twitter authorization

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,5 +1,6 @@
 var passport = require("passport");
 var FacebookStrategy = require("passport-facebook").Strategy;
+var TwitterStrategy = require("passport-twitter").Strategy;
 var User = require("../models/User");
 
 passport.use(new FacebookStrategy({
@@ -9,6 +10,29 @@ passport.use(new FacebookStrategy({
   enableProof: false
   },
   function(access_token, refreshToken, profile, done) {
+    User.findOrCreate({
+      _id: profile.id
+    }, {
+      displayName: profile.displayName
+    }, function(err, user) {
+      if (user) {
+        user.access_token = access_token;
+        user.save(function(err, savedUser) {
+          done(err, savedUser);
+        });
+      } else {
+        done(err, user);
+      }
+    });
+  })
+);
+
+passport.use(new TwitterStrategy({
+    consumerKey: process.env.TWITTER_ID,
+    consumerSecret: process.env.TWITTER_SECRET,
+    callbackURL: "http://localhost:3000/auth/twitter/callback"
+  },
+  function(access_token, tokenSecret, profile, done) {
     User.findOrCreate({
       _id: profile.id
     }, {
@@ -36,11 +60,18 @@ passport.deserializeUser(function(obj, done) {
 
 module.exports = function(router) {
 
-  router.get("/facebook",
-    passport.authenticate("facebook"));
+  router.get("/facebook", passport.authenticate("facebook"));
 
   router.get("/facebook/callback",
     passport.authenticate("facebook", {failureRedirect: "/"}),
+    function(req, res) {
+      res.redirect("/#/success?access_token=" + req.user.access_token);
+    });
+
+  router.get("/twitter", passport.authenticate("twitter"));
+
+  router.get("/twitter/callback",
+    passport.authenticate("twitter", {failureRedirect: "/"}),
     function(req, res) {
       res.redirect("/#/success?access_token=" + req.user.access_token);
     });
